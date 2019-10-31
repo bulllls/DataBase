75781731af5fa76079664ddb34a6ebd0bdc530b9
Ruslan <ruslanfilistovich@gmail.com>
2019-10-31 21:09:45 +0300

    Initial Commit

---
+++ DataBase/AppDelegate.swift
@@ -0,0 +1,87 @@
+//
+//  AppDelegate.swift
+//  DataBase
+//
+//  Created by Ruslan on 10/31/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "DataBase")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
+
+
+
+//Доброе утро. По домашке (кому интересно 🙂 ) мы уже обсуждали, но еще раз повторюсь.
+///Делаем список компаний, у каждой компании есть название и список сотрудников. На экране со списком можно добавить компанию, либо удалить по свайпу. При выборе компании отображается список сотрудников (У сотрудника есть имя и возраст) по убыванию возраста (от старшего к младшему), можно добавить сотрудника в компанию, либо удалить сотрудника.  Напротив каждого сотрудника есть кнопки +/- которые увеличивают/уменьшают возраст сотрудника на 1. Все изменения должны быть синхронизированы с контекстом и проходить автоматически. При выборе сотрудника отображаем информацию о нем, и так же делаем кнопку удалить, при удалении сотрудника на этом экране, при возвращении обратно в список, сотрудника уже не должно быть в списке.
---
+++ DataBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataBase
+//
+//  Created by Ruslan on 10/31/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
